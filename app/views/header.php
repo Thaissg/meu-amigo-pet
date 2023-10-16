7ffec293f5b23afc37e2634faa05bb3097e83bc8
Thaissg <thais_santosg@hotmail.com>
2023-10-16 12:01:54 -0400

    incluido menu responsivo

--- app/views/header.php
+++ app/views/header.php
@@ -1,15 +1,18 @@
+<link rel="stylesheet" href="<?= BASEPATH ?>public/css/style-header.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
 <nav>
-    <div class="logo-container">
-        <img class="logo" src="<?= BASEPATH  ?>public/images/Logo Meu amigo pet.png" alt="Logo Meu amigo pet">
-    </div>
-
-    <div class="menu-superior">
-        <ul class="lista-links">
-            <li><a class="link-menu-superior" href="<?= BASEPATH  ?>home"> Home </a></li>
-            <li><a class="link-menu-superior" href="<?= BASEPATH  ?>adote">Adote</a></li>
-            <li><a class="link-menu-superior" href="<?= BASEPATH  ?>ongs-e-protetoras">ONGs e
-                    Protetores(as)</a></li>
-            <li><a class="link-menu-superior" href="<?= BASEPATH  ?>login">Login</a></li>
-        </ul>
-    </div>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
+    <label class="logo">
+        <img class="logo" src="<?= BASEPATH ?>public/images/Logo Meu amigo pet.png" alt="Logo Meu amigo pet">
+    </label>
+    <ul>
+        <li><a id="home" class="" href="<?= BASEPATH ?>home" onclick="ativar(this.id)"> Home </a></li>
+        <li><a id="adote" class="" href="<?= BASEPATH ?>adote" onclick="ativar(this.id)">Adote</a></li>
+        <li><a id="ongs-protetores" class="" href="<?= BASEPATH ?>ongs-e-protetoras"  onclick="ativar(this.id)">ONGs e
+                Protetores(as)</a></li>
+        <li><a id="login" class="" href="<?= BASEPATH ?>login"  onclick="ativar(this.id)">Login</a></li>
+    </ul>
 </nav>
